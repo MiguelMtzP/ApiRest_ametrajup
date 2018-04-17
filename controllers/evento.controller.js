4f9cd4f64f0f42314b5a0c1cedc5581d3fa6fc2d
Miguel Mtz <miguel_mtz_p@outlook.com>
2018-04-16 23:51:16 -0500

    modulo de eventos completado

--- controllers/evento.controller.js
+++ controllers/evento.controller.js
@@ -1,8 +1,11 @@
 "use strict"
 const Evento = require("../models/evento.model")
 const Galeria = require("../models/galeria.model")
+
+
 function getEventos(peticion,respuesta) {
-    Evento.find({},{},(err,resultado)=>{
+    Evento.find().sort({fecha:1})
+    .exec((err,resultado)=>{
         if (err) {
             respuesta.status(500).send({"err":err})
         } else {
@@ -34,8 +37,61 @@ function crearEvento(req,res) {
     })
 }
 
+function actualizaEnvento(req,res) {
+    let idEvento = req.params.idEvento
+    let params = req.body
+    let evento = {}
+    if (params.titulo){
+        evento.titulo = params.titulo
+    }
 
+    if (params.descripcion){
+        evento.descripcion= params.descripcion
+    }
+
+    if (params.fecha){
+        evento.fecha = params.fecha
+    }
+
+    Evento.findByIdAndUpdate(idEvento,{$set:evento},{new:true})
+    .exec((err,result)=>{
+        if (err) {
+            res.status(500).send({err:err.message})
+        } else {
+            res.status(200).send({eventoActualizado:result})            
+        }
+    })
+}
+
+function EliminarEvento(req,res) {
+    let idEvento = req.params.idEvento
+    Evento.findByIdAndRemove(idEvento)
+    .exec((err,result)=>{
+        if (err) {
+            res.status(500).send({err:err.message})
+        } else if(!result){
+            res.status(500).send({err:"Evento no encontrado"})
+        }else{
+            res.status(200).send({exito:"evento eliminado con exito"})            
+        }
+    })
+}
+
+function getEventoById(req,res) {
+    let id = req.params.idEvento
+    Evento.findById(id)
+    .exec((err,resultado)=>{
+        if (err) {
+            res.status(500).send({err:err.message})
+        } else {
+            res.status(200).send({evento:resultado})            
+        }
+    })
+}
 module.exports = {
     getEventos,
-    crearEvento
+    crearEvento,
+    actualizaEnvento,
+    EliminarEvento,
+    getEventoById
 }
--- routes/evento.route.js
+++ routes/evento.route.js
@@ -4,7 +4,10 @@ const express = require('express');
 var api= express.Router();
 const middlewareAuth = require ("../middlewares/authentication")
 const EventoController = require("../controllers/evento.controller")
+api.get("/:idEvento",EventoController.getEventoById)
 api.get("/",EventoController.getEventos)
+api.delete("/:idEvento",middlewareAuth.authenticate("jwt",{session:false}),EventoController.EliminarEvento)
+api.put("/:idEvento",middlewareAuth.authenticate("jwt",{session:false}),EventoController.actualizaEnvento)
 api.post("/",middlewareAuth.authenticate("jwt",{session:false}),EventoController.crearEvento)
 
 
